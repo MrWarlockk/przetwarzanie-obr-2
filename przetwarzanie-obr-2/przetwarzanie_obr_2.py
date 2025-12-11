5df1b746bec3ea776f43f28808570b5aa789a02e
MrWarlockk <gnatmaciej@gmail.com>
2025-12-11 19:15:49 +0100

    Oblicznie jakosci obrazu Poprawa jakosci obrazu

--- przetwarzanie-obr-2/przetwarzanie_obr_2.py
+++ przetwarzanie-obr-2/przetwarzanie_obr_2.py
@@ -1,4 +1,5 @@
 import io
+from re import S
 import requests
 from PIL import Image
 import numpy as np
@@ -6,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 def downloadImg():
-    imgUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2a/STS069-302-010_-_STS-069_-_Underexposed_-_DPLA_-_d4a340457c33446bd344a121dde4e385.jpg"
+    imgUrl = "https://upload.wikimedia.org/wikipedia/commons/7/77/Magic_lilies_lycoris_squamigera-_underexposed_fill_flash-_indianapolis_in_usa_%2811%29.JPG"
     header = {"User-Agent": "download-img"}
     response = requests.get(imgUrl, headers=header, timeout=15)
     response.raise_for_status()
@@ -46,24 +47,65 @@ def showHistogram(img):
 
     plt.show()
 
+def calculateScore(img):
+    imgArr = np.array(img)
+    imgLumin = (0.2126 * imgArr[:,:,0] + 0.7152 * imgArr[:,:,1] + 0.0722 * imgArr[:,:,2] ).astype(np.uint8)
+    pixelCount = imgLumin.size
+
+    nearZero = np.sum(imgLumin < 10)
+    near255 = np.sum(imgLumin > 245)
+
+    if(nearZero | near255) < (0.05 * pixelCount):
+        return 0
+    else:
+        if(nearZero | near255) < (0.1 * pixelCount):
+            return 1
+        else:
+            return 2
+
+def fixExposure(img, score):
+    if score == 0:
+        return img
+
+    if score == 1:
+        gamma = 0.8
+
+    if score == 2:
+        gamma = 0.6
+
+    imgArr = np.array(img).astype(np.float32) / 255.0
+    imgArr = np.power(imgArr, gamma)
+    imgArr = (imgArr * 255).astype(np.uint8)
+    return Image.fromarray(imgArr)
+
+
 
 
 def main():
     try:
         imgRet = downloadImg()
     except Exception as e:
-        print(f"Error downloading image: {e}")
+        print(f"Blad podczas pobierania obrazu: {e}")
         return
-    print("Image downloaded successfully.")
+    print("Obraz pobrany poprawnie.")
     try:
         img = Image.open(io.BytesIO(imgRet)).convert("RGB")
     except Exception as e:
-        print(f"Error opening image: {e}")
+        print(f"Blad podczas wczytywania obrazu: {e}")
         return
 
-    print("Image loaded successfully.")
+    print("Obraz wczytany pomyslnie.")
     img.show()
 
+    print("Ocena jakosci obrazu (ekspozycja):")
+    score = calculateScore(img)
+
+    print("Poprawa ekspozycji obrazu (jezeli jest taka potrzeba)...")
+    img = fixExposure(img, score)
+    img.show()
+
+    print(f"Wynik oceny: {score} (0 - Dobra jakosc obrazu, 1 - Srednia jakosc obrazu, 2 - Zla jakosc obrazu)")
+    calculateScore(img)
     showHistogram(img)
 
 if __name__ == "__main__":
